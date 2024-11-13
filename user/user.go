21b14a962afa9a4df37c6bb934df66ecac8efcbd
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-13 11:35:07 -0500

    Implemented basic user model. As of right now this stores the decks, sets, and cards the user creates along with basic metadata about the user

--- user/user.go
+++ user/user.go
@@ -1,2 +1,10 @@
 package user
 
+type User struct {
+	Username     string   `json:"username"`
+	Email        string   `json:"email"`
+	CredentialId string   `json:"credentialId"`
+	Decks        []string `json:"decks"`
+	Cards        []string `json:"cards"`
+	Sets         []string `json:"sets"`
+}
