ac3aa86df44fe5827d746771ca8c2a2515323567
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-13 11:42:36 -0500

    Defined the structure for a users credentials

--- user/credentials.go
+++ user/credentials.go
@@ -1,3 +1,7 @@
 package user
 
-
+type UserCredentials struct {
+	CredentialId      string `json:"credentialId"`
+	AESPrivateKey     string `json:"aesPrivateKey"`
+	EncryptedPassword string `json:"encryptedPassword"`
+}
