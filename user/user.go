2ec9ef77d431aa0630c5e2c3374217e1cc40d8a7
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-18 10:08:54 -0500

    Added an additional field to store the users auth0 id in

--- user/user.go
+++ user/user.go
@@ -3,6 +3,7 @@ package user
 type User struct {
 	Username      string   `json:"username"`
 	Email         string   `json:"email"`
+	Auth0Id       string   `json:"auth0Id`
 	LastLoginTime string   `json:"lastLoginTime"`
 	Decks         []string `json:"decks"`
 	Cards         []string `json:"cards"`
